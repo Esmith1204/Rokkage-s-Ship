41ac5bc19922e71a48d43986fd8934f978e46a11
Elijah <elijahiya1204@gmail.com>
2025-09-20 03:36:10 -0700

    Initial commit

---
+++ rokkages-ship/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar/navbar";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+      
+    </>
+  );
+}
+
+export default App;
