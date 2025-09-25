1c47e2d0e95d0742756a6470a3a89b5a42cca2ca
Elijah <elijahiya1204@gmail.com>
2025-09-25 13:30:17 -0700

    Text Changes
    Text changes made to the landing page.

--- rokkages-ship/src/components/Navbar/Navbar.jsx
+++ rokkages-ship/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
                 </button>
             </div>
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Rokkage's Ship</a>
             </div>
             <div className="flex-none">
                 <button className="btn btn-square btn-ghost">
--- rokkages-ship/src/pages/Home/Home.jsx
+++ rokkages-ship/src/pages/Home/Home.jsx
@@ -3,12 +3,12 @@ export default function Home() {
         <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Poop</h1>
+            <h1 className="text-5xl font-bold">Rokkage's Ship</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
               quasi. In deleniti eaque aut repudiandae et a id nisi.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="btn btn-primary">My Projects</button>
           </div>
         </div>
       </div>
